4f68c99f2253f6818c62ab6c93710e2091ef5d6d
jyh <it1468@gsitm.com>
2021-09-17 14:31:22 +0900

    최초커밋

---
+++ src/main/java/com/oauth2/SpringBootOauth2V31Application.java
@@ -0,0 +1,13 @@
+package com.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2V31Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOauth2V31Application.class, args);
+	}
+
+}
